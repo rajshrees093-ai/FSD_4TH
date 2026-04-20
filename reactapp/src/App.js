9ecc5f0b50baac49f846489089f6144c750e69ef
rajshrees093-ai <rajshrees093@gmail.com>
2026-04-20 13:06:33 +0530

    modified

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,1 +1,28 @@
-import React ,{usestate} from "react"
+import React ,{usestate} from "react";
+
+function app(){
+  //to store task
+
+  const[tasks, setTasks]=usestate([]);
+  //state to store input value
+  const[input,setInput]=usestate("");
+  //add task
+  const addTask=()=>{
+    if(input.trim()==="") return;
+
+    const newTask={
+      id:Date.now(),
+      text:input,
+      completed:false,  //in starting we keep the complete status of the task as false
+    };
+
+    setTasks([...tasks.newTask]); // ... separate operator ie add new task without removing the old data
+    setInput("");
+
+    //delete task
+    const deleteTasks=(id)=>{
+      setTasks(tasks.filter((task)=>task.id!==id));
+    }
+
+  }
+}
